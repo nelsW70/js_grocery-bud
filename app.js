27abbef94ad95a9668321769d121687cfeff5dff
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-27 17:59:50 -0500

    add: removeFromLocalStorage

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ function deleteItem(e) {
   displayAlert('item removed', 'danger');
   setBackToDefault();
   // remove from local storage
-  // removeFromLocalStorage(id)
+  removeFromLocalStorage(id);
 }
 // edit function
 function editItem(e) {
@@ -126,15 +126,25 @@ function setBackToDefault() {
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
   const grocery = { id, value };
-  let items = localStorage.getItem('list')
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
-  console.log(items);
+  let items = getLocalStorage();
   items.push(grocery);
   localStorage.setItem('list', JSON.stringify(items));
 }
 
-function removeFromLocalStorage(id) {}
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+  items = items.filter(function (item) {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem('list', JSON.stringify(items));
+}
 function editLocalStorage(id, value) {}
+function getLocalStorage() {
+  return localStorage.getItem('list')
+    ? JSON.parse(localStorage.getItem('list'))
+    : [];
+}
 
 // ****** SETUP ITEMS **********
