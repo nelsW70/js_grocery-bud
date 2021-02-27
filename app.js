944334fb171c81dca15073afdce57c4cc17bb08d
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-27 15:08:16 -0500

    add: display alert

--- app.js
+++ app.js
@@ -21,11 +21,25 @@ function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if (value !== '' && editFlag === false) {
+  if (value && !editFlag) {
     console.log('add item to the list');
-  } else if (value !== '' && editFlag === true) {
+  } else if (value && editFlag) {
     console.log('editing');
-  } else console.log('empty value');
+  } else {
+    displayAlert('please enter value', 'danger');
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
 
 // ****** LOCAL STORAGE **********
