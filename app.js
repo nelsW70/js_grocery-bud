c70f852f29f1762ff5bad4841c4000b8c58a3a54
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-27 17:29:22 -0500

    add: delete functionality

--- app.js
+++ app.js
@@ -89,8 +89,17 @@ function clearItems() {
   // localStorage.removeItem('list')
 }
 // delete function
-function deleteItem() {
-  console.log('item deleted');
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove('show-container');
+  }
+  displayAlert('item removed', 'danger');
+  setBackToDefault();
+  // remove from local storage
+  // removeFromLocalStorage(id)
 }
 // edit function
 function editItem() {
@@ -105,7 +114,8 @@ function setBackToDefault() {
 }
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
-  console.log('added to local storage');
+  // console.log('added to local storage');
 }
 
+function removeFromLocalStorage(id) {}
 // ****** SETUP ITEMS **********
